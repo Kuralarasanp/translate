4f767697de1cbd07c750791c6331c16f111bdd74
Kuralarasanp <kuralarasanp@POCONNOR.COM>
2026-08-03 11:18:30 +0530

    Update Translate.py

--- Translate.py
+++ Translate.py
@@ -3,35 +3,37 @@ import streamlit as st
 import tempfile
 import os
 
+st.set_page_config(page_title="Translate to English")
+
 st.title("🎤 Translate to English")
+
+@st.cache_resource
+def load_model():
+    return WhisperModel(
+        "small",
+        device="cpu",
+        compute_type="int8"
+    )
 
 uploaded_file = st.file_uploader(
     "Upload Audio",
     type=["wav", "mp3", "m4a", "ogg"]
 )
 
-if uploaded_file:
+if uploaded_file is not None:
     with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as f:
         f.write(uploaded_file.read())
         audio_path = f.name
 
-    @st.cache_resource
-    def load_model():
-        return WhisperModel(
-            "small",
-            device="cpu",
-            compute_type="int8"
-        )
-
     model = load_model()
 
-    with st.spinner("Translating... Please wait."):
+    with st.spinner("Translating... Please wait..."):
         segments, info = model.transcribe(
             audio_path,
             task="translate",
             language="es"
         )
-      
+
     text = "".join(segment.text for segment in segments)
 
     st.success("Completed")
